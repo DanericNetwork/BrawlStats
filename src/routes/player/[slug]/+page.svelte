8b6be86b25b48d10a0a8c8645accee0b0c6f1fa5
Daneric <danericaalbers@hotmail.com>
2023-08-21 17:39:55 +0200

    feat: player page

--- src/routes/player/[slug]/+page.svelte
+++ src/routes/player/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+    import { onMount } from "svelte";
     import type { Player, BrawlerProps } from "$lib/playerTypes";
     let playerData: Player;
 
@@ -9,31 +9,71 @@
 
     onMount(() => {
         fetchData();
-    })
-  
+    });
+
     async function fetchData() {
-      if (!playerTag) return alert("Please enter a player tag");
-  
-      if (playerTag.startsWith("#")) playerTag = await playerTag.slice(1);
-  
-      try {
-        const response = await fetch(`/api/getData/${playerTag}`);
-        const data = await response.json();
-        console.log("Data:", data);
-        playerData = data;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+        if (!playerTag) return alert("Please enter a player tag");
+
+        if (playerTag.startsWith("#")) playerTag = await playerTag.slice(1);
+
+        try {
+            const response = await fetch(`/api/getData/${playerTag}`);
+            const data = await response.json();
+            console.log("Data:", data);
+            playerData = data;
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
     }
-  </script>
-  
-  <main>
-    {#if playerData}
-      <h1>{playerData.name}</h1>
-    {:else}
-      <h1>Please enter a player tag!</h1>
-    {/if}
-    <h1>Player Data:</h1>
-    <pre>{JSON.stringify(playerData, null, 2)}</pre>
-  </main>
-  
+</script>
+
+<main>
+    <div class="container">
+        {#if playerData}
+            <div class="profile-card">
+                <img src="/assets/profile/{playerData.icon.id}.png" alt="" />
+                <div class="profile-card-info">
+                    <h2
+                        style="color: #{playerData.nameColor.replace(
+                            '0xff',
+                            ''
+                        )}"
+                    >
+                        {playerData.name}
+                    </h2>
+                    <p>{playerData.tag}</p>
+                </div>
+            </div>
+            <div class="brawler-container">
+                <h2>Brawlers</h2>
+                <div class="brawlers-list">
+                    {#each playerData.brawlers as brawler}
+                        <div class="brawler-card">
+                            <div class="brawler-card-header">
+                                <div class="brawler-card-header-rank">
+                                    {#if brawler.rank > 0}
+                                        <img
+                                            src="/assets/rank/{brawler.rank}.png"
+                                            alt="rank-{brawler.rank}"
+                                        />
+                                    {/if}
+                                    <p class="rank">{brawler.trophies}</p>
+                                </div>
+                                <p class="title">{brawler.name}</p>
+                                <div class="brawler-card-header-rank">
+                                    <p class="rank">{brawler.power}</p>
+                                    <img
+                                        src="/assets/PowerPoint.png"
+                                        alt="powerpoint"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    {/each}
+                </div>
+            </div>
+        {:else}
+            <h1>Loading Data...</h1>
+        {/if}
+    </div>
+</main>
